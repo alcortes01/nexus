f5d28963e3dae43648305c19cbd912f624b58cd6
Alberto Cortes <alcortes01@gmail.com>
2019-06-28 23:23:13 -0400

    License information

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# artifactory_oss
+# nexus
 
-TODO: Enter the cookbook description here.
+Chef cookbook to install Sonatype Nexus
 
+List of attributes to 
+modify in attributes/default.rb:
+
+```
+default['nexus']['rhel_java_version'] = 'java-1.8.0-openjdk-headless'
+
+default['nexus']['version'] = 'nexus-3.17.0-01'
+```
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'nexus'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Alberto Cortes'
+maintainer_email 'alcortes01@gmail.com'
+license 'MIT'
 description 'Installs/Configures nexus'
 long_description 'Installs/Configures nexus'
 version '0.1.0'
@@ -11,10 +11,10 @@ chef_version '>= 14.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/artifactory_oss/issues'
+issues_url 'https://github.com/alcortes01/nexus/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/artifactory_oss'
+source_url 'https://github.com/alcortes01/nexus'
