4c3eb0549d1d9b597fb8224eb94e01eafe4101a7
Alberto Cortes <alcortes01@gmail.com>
2019-06-28 23:32:07 -0400

    version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,17 +4,13 @@ maintainer_email 'alcortes01@gmail.com'
 license 'MIT'
 description 'Installs/Configures nexus'
 long_description 'Installs/Configures nexus'
-version '0.1.0'
-chef_version '>= 14.0'
+version '1.0.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
+chef_version '>= 14.0'
+%w(centos redhat).each do |os|
+    supports os
+end
+
 issues_url 'https://github.com/alcortes01/nexus/issues'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
 source_url 'https://github.com/alcortes01/nexus'
